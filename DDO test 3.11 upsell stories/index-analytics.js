131e6cc4d99c5c60a8da68d07e51427fc92f9675
Abhishek Bhatt <abhbhatt5@publicisgroupe.net>
2019-10-10 16:01:42 +0530

    analytics-upsell

---
+++ DDO test 3.11 upsell stories/index-analytics.js
@@ -0,0 +1,84 @@
+(function() {
+    if (!window.hasUpSellFlagSet) {
+        var mtObserver = new MutationObserver(function(mvt, observer) {
+            setTimeout(function() {                           
+                var tiles = $('service-tile > div > input[id^="nbsServiceTileServiceRadio"]');
+                if (tiles && tiles.length > 0) {
+                    var selectedTiles = $('service-tile > div > input[id^="nbsServiceTileServiceRadio"]:checked')[0];
+                    if (selectedTiles) {
+                        setDefaultFlagWithTiles();
+                    }
+                }
+            }, 0);
+        });
+
+        var config = {
+            childlist: true,
+            subtree: true,
+            attributes: true
+        };
+        mtObserver.observe(document.body, config);
+
+        function setDefaultFlagWithTiles() {
+            var selectedTiles = $('service-tile > div > input[id^="nbsServiceTileServiceRadio"]:checked+div')[0];
+            if (selectedTiles && selectedTiles.innerText) {
+                if (selectedTiles.innerText === 'Preferred' || selectedTiles.innerText === 'Faster' || selectedTiles.innerText === 'Fastest') {
+                    window.selectedTiles = selectedTiles.innerText;
+                } else {
+                    window.selectedTiles = null;
+                }
+            }
+        }
+
+        function setSelectedtiles() {
+            var selectedTiles = event.target.parentElement.querySelector('input+div');
+            if (selectedTiles && selectedTiles.innerText) {
+                if (selectedTiles.innerText === 'Preferred' || selectedTiles.innerText === 'Faster' || selectedTiles.innerText === 'Fastest') {
+                    window.selectedTiles = selectedTiles.innerText;
+                } else {
+                    window.selectedTiles = null;
+                }
+            }
+        }
+
+        $(document).on("change", ".upsell-tiles", function(event) {
+            if (event.target.checked) {
+                setSelectedtiles();
+            }
+        });
+
+        $(document).on("change", ".ups-shipping_schedule_grid", function(event) {
+            if (event.target.checked) {
+                setSelectedtiles();
+            }
+
+        });
+
+        $(document).on("click", "button[id='nbsBackForwardNavigationReviewPrimaryButton']", function(event) {
+            if (!window.selectedTiles) {
+                setDefaultFlagWithTiles();
+            }
+
+        });
+
+        $(document).on("click", "button[id='nbsBackForwardNavigationReviewSecondaryButton']", function(event) {
+             if (!window.selectedTiles) {
+                setDefaultFlagWithTiles();
+            }
+
+        });
+
+        $(document).on("click", "button[id='nbsBackForwardNavigationPayAndGetLabelsButton']", function(event) {
+            if (window.selectedTiles) {
+                utag.link({
+                    "service_label": window.selectedTiles,
+                    "link_name": 'Service label selected',
+                    "link_page_name": utag.data['new_page_name']
+                });
+            }
+        });
+        
+        window.hasUpSellFlagSet = true;
+    }
+}
+)();
