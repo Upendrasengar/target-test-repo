865a71a62e1f84288f47092d9e8600163bf28b4f
Abhishek Bhatt <abhbhatt5@publicisgroupe.net>
2019-11-07 17:57:03 +0530

    promotional 4.1 changes

--- DDO test 4.1 promotion offer/single-page/index-v1.js
+++ DDO test 4.1 promotion offer/single-page/index-v1.js
@@ -1,6 +1,6 @@
 (function() {
     if (!window.hasPromotionalCodeSet) {
-        var modalHtml = '<div id="promotional_modal" aria-hidden="false" class="modal fade ups-form_wrap in" role="dialog" style="display: none;" tabindex="-1"><div class="modal-dialog modal-lg" role="document"><div class="modal-content"><div class="modal-header"><!----><button id="closePromotionalModal"class="close ng-star-inserted" data-dismiss="modal"><span aria-hidden="true" class="ups-icon-x"></span><span class="ups-readerTxt">Close</span></button><h2 class="modal-title">Get up to 40% off!<h2><p style="display:inline-block;">When you use the "Easy" promotion code</p></div><div class="modal-body ups-form_wrap"> <div></div> <div> Nunce Congue lacus tincident portitor posuere. </div> <div class="ups-clear ups-form_ctaGroup text-center ups-noTxt"><button class="ups-cta ups-cta_primary" id="applyPromotionalCode" type="button">Apply Code</button></div></div></div></div></div><div id="promotional_overlay" class="modal-backdrop fade in"></div>';
+        var modalHtml = '<div id="promotional_modal" aria-hidden="false" class="modal fade ups-form_wrap in" role="dialog" style="display: none;" tabindex="-1"><div class="modal-dialog modal-lg" role="document"><div class="modal-content"><div class="modal-header"><!----><button id="closePromotionalModal"class="close ng-star-inserted" data-dismiss="modal"><span aria-hidden="true" class="ups-icon-x"></span><span class="ups-readerTxt">Close</span></button><h2 class="modal-title" id="promotional_offer_header">Get up to 40% off!<h2><p style="display:inline-block;" id="promotional_offer_paragraph">When you use the "Easy" promotion code</p></div><div class="modal-body ups-form_wrap"> <div></div> <div id="promotional_offer_body"> Nunce Congue lacus tincident portitor posuere. </div> <div class="ups-clear ups-form_ctaGroup text-center ups-noTxt"><button class="ups-cta ups-cta_primary" id="applyPromotionalCode" type="button">Apply Code</button></div></div></div></div></div><div id="promotional_overlay" class="modal-backdrop fade in"></div>';
 
         var css = "<style id='Test_4.1'>\
         #promotional_modal{\
@@ -37,7 +37,7 @@
                 subtree: true
             };
             var callback = function(mutations, observer) {
-               if (location.href.indexOf('promoCodeAlias') < 0 && document.querySelector("payment promo-code label[for='nbsPromoCodeUsePromoCodeSwitch']")) {
+                if (location.href.indexOf('promoCodeAlias') < 0 && document.querySelector("payment promo-code label[for='nbsPromoCodeUsePromoCodeSwitch']")) {
                     setTimeout(function() {
                         if (window.promotionalCode) {
                             applyPromoCodeInPayments();
@@ -50,18 +50,23 @@
         }
 
         function openPromotionalModal() {
-            if(displayModal()) {
-                var modal = document.getElementById("promotional_modal");
-                if (!modal) {
-                    $('body').append(modalHtml);
-                    modal = document.getElementById("promotional_modal");
-                }
-                modal.style.display = "block";
-            }     
+            var logged_in = true;
+            var modal = document.getElementById("promotional_modal");
+            if (!modal) {
+                $('body').append(modalHtml);
+                modal = document.getElementById("promotional_modal");
+            }
+            modal.style.display = "block";
+
+            logged_in = window.data_obj && window.data_obj.isLoggedIn || logged_in;
+
+            if(logged_in){
+                modal.querySelector('#promotional_offer_paragraph').innerText = "Signed in user";
+            }  
         }
 
         $(document).on("click", "a", function(event) {
-            if (event.target.tagName === 'A') {
+            if (location.href.indexOf('promoCodeAlias') < 0 && displayModal() && event.target.tagName === 'A') {
                 if (!window.showAlertMessage && event.target.getAttribute('href') && event.target.getAttribute('href').length > 1) {
                     if (event.target.innerText != 'Log In' || event.target.innerText != 'Cancel Shipment') {
                         window.showAlertMessage = true;
@@ -94,14 +99,35 @@
         }
 
         function applyPromoCodeInPayments() {
-            var promoToggle = $("promo-code label[for='nbsPromoCodeUsePromoCodeSwitch']");
-            promoToggle.click();
             var input = document.querySelector("input[id='nbsPromoCodePaymentPromoCode']");
-            if (input && $('#nbsApplyOrRemovePromoCodeBtn').length == 1) {
-                input.value = window.promotionalCode;
-                input.dispatchEvent(getEvent());                                                
-                window.promotionalCode = null;                                  
+            var promoToggle = $("promo-code label[for='nbsPromoCodeUsePromoCodeSwitch']");
+            if (window.promotionalCode && $('#nbsPromoCodeUsePromoCodeSwitch').length == 1) {
+                if (input && input.value.length === 0) {
+                    if (input && $('#nbsApplyOrRemovePromoCodeBtn').length == 1) {
+                        input.value = window.promotionalCode;
+                        input.dispatchEvent(getEvent());
+                        document.addEventListener("change", cancelShipmentHandler, true);                        
+                        setTimeout(function(){
+                           $('#nbsApplyOrRemovePromoCodeBtn').click(); 
+                        },300);
+                        function cancelShipmentHandler(event) {
+                            if (event.target.id == "nbsPromoCodeUsePromoCodeSwitch" || event.target.id == "nbsCancelShipmentWarningYes" || event.target.id == "nbsButtonDrawer4" || event.target.id === "nbsApplyOrRemovePromoCodeBtn") {
+                                window.promotionalCode = null;
+                            }
+                        }
+                        document.addEventListener("click", function(event) {
+                            if (event.target.parentElement && event.target.parentElement.id === "nbsApplyOrRemovePromoCodeBtn" && event.target.classList.contains('ups-icon-minuscircle')) {
+                                window.promotionalCode = null;
+                            }
+                        }, true);
+                    }
+
+                } else if (!input) {
+                    promoToggle.click();
+                }
+
             }
+
         }
 
         function getEvent() {
@@ -111,24 +137,23 @@
                 var ev = document.createEvent("Event");
                 ev.initEvent("input", false, true);
                 return ev;
-            }
-            else {
+            } else {
                 return new Event("input");
             }
         }
 
         function displayModal() {
             var flag = false;
-            if(location.href.indexOf('guided') >0)  {
-                if(location.href.indexOf('ship/guided/pickup-service') >0 || location.href.indexOf('ship/guided/options') >0 || location.href.indexOf('ship/guided/payment') >0) {
-                    flag =  true;
-                }                                
-            } else if(location.href.indexOf('single-page') >0) {
+            if (location.href.indexOf('guided') > 0) {
+                if (location.href.indexOf('ship/guided/pickup-service') > 0 || location.href.indexOf('ship/guided/options') > 0 || location.href.indexOf('ship/guided/payment') > 0) {
+                    flag = true;
+                }
+            } else if (location.href.indexOf('single-page') > 0) {
                 flag = true;
-            } 
+            }
             return flag;
         }
-        
+
         window.hasPromotionalCodeSet = true;
     }
 }
